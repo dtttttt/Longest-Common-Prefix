95954eabdd094ec94bd12b3a7c90347c5b6b123e
dtttttt <47856979+dtttttt@users.noreply.github.com>
2019-03-09 16:05:42 +0800

    3th
    这个vs通过了但是里扣通不过，实在找不到原因，先存档

--- Longest Common Prefix.cpp
+++ Longest Common Prefix.cpp
@@ -1,16 +1,34 @@
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-    if (strs.empty())
-			return "";
-    sort(strs.begin(), strs.end());
-	int len = std::min(strs[0].length(), strs.back().length());
-	for (int i = 0; i < len; i++)
+string longestCommonPrefix(std::vector<string>& strs) {
+	if (strs.empty())
+		return "";
+	string str1 = strs.back();
+	string strResu;
+	int VecSize = strs.size(); 
+	for (int i = 0; i < VecSize - 1; i++)
 	{
-		if (i>=len || strs[0][i] != strs.back()[i])
-			return strs[0].substr(0, i);
-		
+		string str2 = strs.at(strs.size() - 2);
+		while (str1.front() == str2.front())
+		{
+			strResu.push_back(str1.front());
+			str1.erase(str1.begin());
+			str2.erase(str2.begin());
+			if (str1.length() == 0||str2.length()==0)//这肯定没有限制完全？？
+			{
+				break;
+			}
+			
+		}
+		if (strResu == "")
+		{
+			return "";
+		}
+		else
+		{
+			str1 = strResu;
+			strResu.clear();
+		}
+		strs.pop_back();
 	}
-	return strs[0];
-    }
+	return str1;
+
 };
